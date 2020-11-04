cddd45a349fb1ac94693f17110c9615a215fd4e3
Carlos <carlos.gomez2500@gmail.com>
2020-11-04 19:58:08 +0100

    Initial commit

---
+++ src/elpuig/Productos.java
@@ -0,0 +1,39 @@
+package elpuig;
+
+public class Productos {
+    int cartaDeProductos;
+    String bacon;
+    String panini;
+    String tortilla;
+    String frankfurt;
+    String vegetal;
+    String cafe;
+    String cortado;
+    String cafeConHielo;
+    String triangulo;
+    String cacaolat;
+    String zumo;
+//por encargo
+    String pollo;
+    String baconQueso;
+
+    public void cartaDeProductos() {
+        System.out.println("1.Bocadillo de bacon");
+        System.out.println("2.Panini");
+        System.out.println("3.Bocadillo de tortilla");
+        System.out.println("4.Bocadillo     de frankfurt");
+        System.out.println("5.Bocadillo vegetal");
+        System.out.println("6.Café");
+        System.out.println("7.Cortado");
+        System.out.println("8.Café con hielo");
+        System.out.println("9.Triangulo");
+        System.out.println("10.Cacaolat");
+        System.out.println("11.Zumo");
+
+        System.out.println("POR ENCARGO");
+        System.out.println("1.Bocadillo de pollo");
+        System.out.println("2.Bocadillo de bacon con queso");
+
+
+    }
+}
